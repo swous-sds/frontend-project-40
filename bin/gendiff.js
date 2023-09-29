e9322292a37701a65fa67615cd596d245c38405d
swous-sds <sonyakrvnk@gmail.com>
2023-09-29 17:04:43 +0500

    step 1

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env node
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .description('CLI to some JavaScript string utilities')
+    .version('0.8.0');
+
+program.parse(process.argv);
